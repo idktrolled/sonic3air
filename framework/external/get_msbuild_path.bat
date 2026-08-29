f3801dcd641d79a5cb45a50624ae86103ea28792
idktrolled <camilocarlos1100@gmail.com>
2026-08-29 01:02:15 -0500

    Refactor MSBuild path retrieval for Visual Studio 2022

--- framework/external/get_msbuild_path.bat
+++ framework/external/get_msbuild_path.bat
@@ -1,30 +1,18 @@
 @echo off
 
-:: GitHub Actions / Visual Studio 2022
-set "vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+set "msbuildPathCom=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+set "msbuildPathPro=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
 
-if not exist "%vswhere%" (
-    echo Error: vswhere.exe not found
-    exit /b 1
+if exist "%msbuildPathCom%" (
+    set "msbuildPath=%msbuildPathCom%"
+) else (
+    if exist "%msbuildPathPro%" (
+        set "msbuildPath=%msbuildPathPro%"
+    ) else (
+        echo Error: No Visual Studio 2022 installation found
+        exit /b 1
+    )
 )
 
-for /f "usebackq tokens=*" %%i in (`"%vswhere%" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-    set "vsPath=%%i"
-)
-
-if not defined vsPath (
-    echo Error: No Visual Studio 2022 installation found
-    exit /b 1
-)
-
-set "msbuildPath=%vsPath%\MSBuild\Current\Bin\MSBuild.exe"
-
-if not exist "%msbuildPath%" (
-    echo Error: MSBuild.exe not found
-    echo Expected: %msbuildPath%
-    exit /b 1
-)
-
-echo Using Visual Studio installation: %vsPath%
-echo Using MSBuild: %msbuildPath%
+echo Using Visual Studio installation: %msbuildPath%
 echo.
