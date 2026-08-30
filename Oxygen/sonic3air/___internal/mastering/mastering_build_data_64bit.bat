c0b7ff4efdeca6fc37b72da060d03f7e66a156b1
idktrolled <camilocarlos1100@gmail.com>
2026-08-29 20:50:44 -0500

    Update mastering_build_data_64bit.bat

--- Oxygen/sonic3air/___internal/mastering/mastering_build_data_64bit.bat
+++ Oxygen/sonic3air/___internal/mastering/mastering_build_data_64bit.bat
@@ -21,15 +21,42 @@ start "" "bin\Release_x64\Sonic3AIR.exe" -pack
 ping 127.0.0.1 -n 21 >nul
 taskkill /IM Sonic3AIR.exe /F /T >nul 2>&1
 
-move "enginedata.bin" "%outputDir%\data"
-move "gamedata.bin" "%outputDir%\data"
-move "audiodata.bin" "%outputDir%\data"
-move "audioremaster.bin" "%outputDir%\data"
-copy "data\metadata.json" "%outputDir%\data" /y
+:: Comprobar la existencia de cada archivo
+if exist "enginedata.bin" (
+    move "enginedata.bin" "%outputDir%\data"
+) else (
+    echo [ERROR] No se encontro enginedata.bin
+)
 
-:: Copy scripts
-:: TODO: Make sure these scripts are really up-to-date
-copy "saves\scripts.bin" "%outputDir%\data"
+if exist "gamedata.bin" (
+    move "gamedata.bin" "%outputDir%\data"
+) else (
+    echo [ERROR] No se encontro gamedata.bin
+)
+
+if exist "audiodata.bin" (
+    move "audiodata.bin" "%outputDir%\data"
+) else (
+    echo [ERROR] No se encontro audiodata.bin
+)
+
+if exist "audioremaster.bin" (
+    move "audioremaster.bin" "%outputDir%\data"
+) else (
+    echo [ERROR] No se encontro audioremaster.bin
+)
+
+if exist "data\metadata.json" (
+    copy "data\metadata.json" "%outputDir%\data" /y
+) else (
+    echo [ERROR] No se encontro data\metadata.json
+)
+
+if exist "saves\scripts.bin" (
+    copy "saves\scripts.bin" "%outputDir%\data" /y
+) else (
+    echo [ERROR] No se encontro saves\scripts.bin
+)
 
 popd
 
